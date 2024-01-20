c9b1868e197132a4bdd01d5562a2522e7634dda3
Simon Chung <stchung4@gmail.com>
2024-01-19 23:23:40 -0600

    Commit Jan 19

--- src/PlatformA.java
+++ src/PlatformA.java
@@ -2,30 +2,7 @@
  * TESTING PLATFORM A
  */
 public class PlatformA {
-    static String convert(int num, int base) {
-        char[] ch = {'0', '1', '2', '3', '4', '5', '6', '7', '8',
-                '9', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
-                'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-        if (num == 0) return "";
-        return convert(num / base, base) + ch[num % base];
-    }
-
-    private static int getValue(char ch) {
-        if (ch >= 'A' && ch <= 'Z') return (int) ch - 55;
-        return Integer.parseInt(String.valueOf(ch));
-    }
-
-    private static long revert(String num, int place_value, int fin, int base) {
-        if (num.isEmpty()) return fin;
-        fin += getValue(num.charAt(num.length() - 1)) * place_value;
-        return revert(num.substring(0, num.length() - 1), place_value * base, fin, base);
-    }
-
-    static long revert(String num, int base) {
-        return revert(num.toUpperCase(), 1, 0, base);
-    }
-
     public static void main(String [ ] args) {
-
+        System.out.println(45 ^ 32); // 101101 100000
     }
 }
--- src/PlatformB.java
+++ src/PlatformB.java
@@ -3,7 +3,7 @@
  */
 public class PlatformB {
     public static void main(String [ ] args) {
-        int[] a = {5, -6, 2, -4, 3, -1, 0, -4};
+        int[] a = {1, -4, 6, 1, -4, -6, 2};
         int max = 0, cur = 0;
         for (int i : a) {
             cur = Math.max(i, i + cur);

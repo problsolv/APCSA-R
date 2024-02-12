f358beac6a0b22a7f4bd4b72e0f768a1ab3f61c5
Simon Chung <stchung4@gmail.com>
2024-02-11 23:22:40 -0600

    Commit Feb 11

--- src/PlatformA.java
+++ src/PlatformA.java
@@ -3,29 +3,29 @@
  */
 public class PlatformA {
     static long step = 0;
-    public static void main(String [ ] args) throws InterruptedException {
-        for (int i = 1; i <= 100; i++) {
-            System.out.println(i + ". " + fibonacci(i) + " -- " + step);
-            step = 0;
-        }
-        System.out.println("\n");
-        for (int i = 1; i <= 50; i++) {
-            System.out.println(i + ". " + f(i) + " -- " + step);
+    public static void main(String [ ] args) {
+        int a = 45;
+        for (int i = 1; i <= a; i++) {
+            System.out.printf("f1(%2d) = %9d -- step = %10d%10s(%2d) = %9d -- step = %2d%n", i, f1(i), step, "f2", i, f2(i), step);
             step = 0;
         }
     }
 
-    static long f(int n) {
+    static long f1(int n) {
         step++;
         if (n <= 2) return n - 1;
-        return f(n - 1) + f(n - 2);
+        return f1(n - 1) + f1(n - 2);
     }
 
-    static long fibonacci(int n) {
-        step += 2;
+    static long f2(int n) {
+        step = 0;
         n--;
-        if (n <= 1) return n;
+        if (n <= 1) {
+            step++;
+            return n;
+        }
         long[] fib = new long[n + 1];
+        step += 2;
         fib[0] = 0;
         fib[1] = 1;
         for (int i = 2; i <= n; i++) {

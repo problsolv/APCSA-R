7f3580a82bb565cc2df63fcb6018b99143db4dff
Simon Chung <stchung4@gmail.com>
2024-02-12 21:20:39 -0600

    Commit Feb 12

--- src/PlatformA.java
+++ src/PlatformA.java
@@ -4,17 +4,16 @@
 public class PlatformA {
     static long step = 0;
     public static void main(String [ ] args) {
-        int a = 45;
+        int a = 50;
         for (int i = 1; i <= a; i++) {
-            System.out.printf("f1(%2d) = %9d -- step = %10d%10s(%2d) = %9d -- step = %2d%n", i, f1(i), step, "f2", i, f2(i), step);
+            System.out.printf("f1(%2d) = %10d -- step = %12d%10s(%2d) = %10d -- step = %2d%n", i, f1(i), step, "f2", i, f2(i), step);
             step = 0;
         }
     }
 
     static long f1(int n) {
         step++;
-        if (n <= 2) return n - 1;
-        return f1(n - 1) + f1(n - 2);
+        return (n <= 2) ? n - 1 : f1(n - 1) + f1(n - 2);
     }
 
     static long f2(int n) {

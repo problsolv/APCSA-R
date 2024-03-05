f92a03bb137b5a53477fa5ecda007d56a4304f64
Simon Chung <stchung4@gmail.com>
2024-03-04 18:56:33 -0600

    Revert "Commit Feb 29"
    This reverts commit de94a1185e7753d5f24ec9d4dcfba5eb75083929.

--- src/PlatformB.java
+++ src/PlatformB.java
@@ -3,10 +3,10 @@
  */
 public class PlatformB {
     static long binaryToDecimal(String str, int val) {
-        return str.isEmpty() ? 0 : (long)((str.charAt(str.length() - 1) - '0') * Math.pow(2, val)) + binaryToDecimal(str.substring(0, str.length() - 1), val + 1);
+        return str.isEmpty() ? 0 : ((long)((str.charAt(str.length() - 1) - '0') * Math.pow(2, val)) + binaryToDecimal(str.substring(0, str.length() - 1), val + 1));
     }
     static String decimalToBinary(long n) {
-        return n == 0 ? "" : decimalToBinary(n / 2) + n % 2;
+        return n == 0 ? "" : (decimalToBinary(n / 2) + (n % 2));
     }
     public static void main(String [ ] args) {
         System.out.println(binaryToDecimal("111111", 0));

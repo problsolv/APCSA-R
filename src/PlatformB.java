38a6cf1d784bdca32e4fc286991b2603e21d6dc3
Simon Chung <stchung4@gmail.com>
2024-07-22 19:04:28 -0500

    Commit Jul 22

--- src/PlatformB.java
+++ src/PlatformB.java
@@ -1,35 +1,20 @@
-import java.util.*;
 /**
  * TESTING PLATFORM B
  */
 public class PlatformB {
     public static void main(String[] args) {
-        Integer[] arr = {5, 20, 3, 2, 50, 80};
-        int n = 78;
-        Arrays.sort(arr);
-        for (Integer i : arr) {
-            int b = binarySearch(arr, n + i);
-            if (b != -1) {
-                System.out.println("(" + i + ", " + arr[b] + ")");
-                return;
-            }
-        }
-        System.out.println("No Such Pair");
+        double n = 96;
+        System.out.println(Math.floor(Math.sqrt(n)));
+        System.out.println(Math.floor(sqrt(n)));
     }
-
-    static int binarySearch(Integer[] arr, int n) {
-        int l = 0, r = arr.length - 1;
-        while (l <= r) {
-            int m = l + (r - l) / 2;
-            if (arr[m] == n) {
-                return m;
-            }
-            if (arr[m] < n) {
-                l = m + 1;
-            } else {
-                r = m - 1;
-            }
+    static double sqrt(double n) {
+        double x = n;
+        double y = 1;
+        double e = 0.000001;
+        while(x - y > e) {
+            x = (x + y) / 2;
+            y = n / x;
         }
-        return -1;
+        return x;
     }
 }

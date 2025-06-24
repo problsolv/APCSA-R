66abd5a35d6c945d8adcc10c4d0077e46e5fb5f4
Simon Chung <stchung4@gmail.com>
2025-06-24 15:54:07 -0500

    Commit Jun 24

---
+++ src/cf/CFSquareYear.java
@@ -0,0 +1,39 @@
+package cf;
+
+import java.io.*;
+import java.util.*;
+
+public class CFSquareYear {
+    public static void main(String [ ] args) {
+        FastScanner sc = new FastScanner(System.in);
+        PrintWriter out = new PrintWriter(System.out);
+        int a = sc.nextInt();
+        while (a-->0) {
+            int n = sc.nextInt();
+            if (Math.sqrt(n) % 1 != 0) out.println(-1);
+            else out.println("0 " + (int)Math.sqrt(n));
+        }
+        out.close();
+    }
+    static class FastScanner {
+        BufferedReader br;
+        StringTokenizer st;
+        FastScanner(InputStream i) { br = new BufferedReader(new InputStreamReader(i)); }
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try { st = new StringTokenizer(br.readLine()); }
+                catch (IOException e) { e.printStackTrace(); }
+            }
+            return st.nextToken();
+        }
+        int nextInt() { return Integer.parseInt(next()); }
+        long nextLong() { return Long.parseLong(next()); }
+        double nextDouble() { return Double.parseDouble(next()); }
+        String nextLine() {
+            String str = "";
+            try { str = br.readLine(); }
+            catch (IOException e) { e.printStackTrace(); }
+            return str;
+        }
+    }
+}

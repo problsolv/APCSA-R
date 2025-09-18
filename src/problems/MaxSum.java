8335338c0918b74396ac5d93c9ebfe39725581c8
Simon Chung <stchung4@gmail.com>
2025-09-18 18:09:51 -0500

    Commit Sep 18

--- src/problems/MaxSum.java
+++ src/problems/MaxSum.java
@@ -3,9 +3,8 @@ import java.util.*;
 public class MaxSum {
     public static void main(String [ ] args) {
         int[] arr = {1, 2, 5, 7, 10, 14, 19, 20, 21, 24};
-        int k = 9;
+        int k = 21;
         sum(arr, k);
-        difference(arr, k);
     }
 
     static void sum(int[] arr, int k) {
@@ -16,31 +15,11 @@ public class MaxSum {
                 System.out.println(arr[left] + ", " + arr[right]);
                 return;
             }
-            else if (arr[right] + arr[left] < k) {
-                left++;
-            }
-            else {
-                right--;
-            }
+            else if (arr[right] + arr[left] < k) left++;
+            else right--;
         }
-        System.out.println("No such pair found");
-    }
-
-    static void difference(int[] arr, int k) {
-        Arrays.sort(arr);
-        int left = 0, right = arr.length - 1;
-        while (left < right) {
-            if (arr[right] - arr[left] == k) {
-                System.out.println(arr[left] + ", " + arr[right]);
-                return;
-            }
-            else if (arr[right] - arr[left] < k) {
-                left++;
-            }
-            else {
-                right--;
-            }
-        }
+        left = 0;
+        right = arr.length - 1;
         System.out.println("No such pair found");
     }
 }

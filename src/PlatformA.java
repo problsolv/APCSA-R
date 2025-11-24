d5b0aa5bbd51eac54e861bb0505bef9b939d71b9
Simon Chung <stchung4@gmail.com>
2025-11-23 21:15:17 -0600

    Commit Sep 18

--- src/PlatformA.java
+++ src/PlatformA.java
@@ -1,31 +1,103 @@
+import java.util.*;
 /**
  * TESTING PLATFORM A
  */
 public class PlatformA {
-    static long step1 = 0, step2 = 0;
-    public static void main(String [ ] args) {
-        for (int i = 0; i <= 50; i++) {
-            System.out.printf("f1(%2d) = %11d -- step = %12d%10s(%2d) = %11d -- step = %2d%n", i, f1(i), step1, "f2", i, f2(i), step2);
-            step1 = 0;
-            step2 = 0;
+
+    static class Node {
+        char val;
+        Node next;
+        Node(char v, Node n) {
+            val = v;
+            next = n;
         }
     }
 
-    static long f1(final int n) {
-        step1++;
-        return (n <= 1) ? n : f1(n - 1) + f1(n - 2);
+    static class Stack implements Iterable<Character> {
+        Node top;
+        void push(char c) {
+            top = new Node(c, top);
+        }
+        char peek() {
+            return top.val;
+        }
+        boolean isEmpty() {
+            return top == null;
+        }
+
+        void pop() {
+            char v = top.val;
+            top = top.next;
+        }
+
+        void clear() {
+            top = null;
+        }
+
+        void duplicate() {
+            if (top == null) return;
+
+            Node cur = top;
+            Node tail = null;
+
+            while (cur != null) {
+                tail = new Node(cur.val, tail);
+                cur = cur.next;
+            }
+
+            Node t = tail;
+            while (t != null) {
+                push(t.val);
+                t = t.next;
+            }
+        }
+
+        public Iterator<Character> iterator() {
+            ArrayList<Character> list = new ArrayList<>();
+            Node cur = top;
+            while (cur != null) {
+                list.add(cur.val);
+                cur = cur.next;
+            }
+            Collections.reverse(list);
+            return list.iterator();
+        }
     }
 
-    static long f2(final int n) {
-        if (n <= 1) {
-            step2++;
-            return n;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Stack stack = new Stack();
+        String st = sc.next();
+
+        for (char c : st.toCharArray()) {
+
+            if (c == '6') stack.push(c);
+
+            else if (c == '7') {
+                if (!stack.isEmpty() && stack.peek() == '6') stack.pop();
+                else stack.push(c);
+            }
+
+            else if (c == 'A') {
+                Node cur = stack.top;
+                Node rev = null;
+                while (cur != null) {
+                    rev = new Node(cur.val, rev);
+                    cur = cur.next;
+                }
+                stack.top = rev;
+            }
+
+            else if (c == 'B') stack.duplicate();
+
+            else if (c == '#') stack.clear();
         }
-        final long[] FIB = new long[n + 1];
-        for (int i = 0; i <= n; i++) {
-            step2++;
-            FIB[i] = (i <= 1) ? i : FIB[i - 1] + FIB[i - 2];
+
+        if (stack.isEmpty()) System.out.println("EMPTY");
+        else {
+            StringBuilder res = new StringBuilder();
+            for (char c : stack) res.append(c);
+            System.out.println(res);
         }
-        return FIB[n];
     }
 }
